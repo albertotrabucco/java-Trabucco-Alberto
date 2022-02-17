800ba92248005c52cbddb709838fdf92a360915c
Alberto <alberto.trabucco97@gmail.com>
2022-02-17 16:53:59 +0100

    test 6

--- es-lezione2/esercizio2.java
+++ es-lezione2/esercizio2.java
@@ -4,8 +4,9 @@ import java.util.Arrays;
 public class esercizio2 {
     public static String checkSort(int[] a){
         String risposta = "";
-        int arrcheckAsc[] = new int[10];
-        int arrcheckDesc[] = new int[10];
+        int n = 3;
+        int arrcheckAsc[] = new int[n];
+        int arrcheckDesc[] = new int[n];
         for(int i = 0; i < a.length ;i++){
             arrcheckAsc[i] = a[i];
             arrcheckDesc[i] = -a[i];
@@ -30,8 +31,9 @@ public class esercizio2 {
     
     
     public static void main(String[] args){
+        int n = 3;
         Scanner keyboard = new Scanner(System.in);
-        int arr[] = new int[10];
+        int arr[] = new int[n];
         for(int i = 0; i < arr.length; i++){
             System.out.print("Inserisci il"+(i+1)+"° numero: ");
             arr[i] = Integer.parseInt(keyboard.nextLine());
