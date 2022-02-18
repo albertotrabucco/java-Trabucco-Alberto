0a639cfdde1eae9ed3cee54fd503e63b60fa5773
Alberto Trabucco <alberto.trabucco97@gmail.com>
2022-02-18 13:34:16 +0100

    Esercizio swap

--- es-lezione3/EsOrdinamento.java
+++ es-lezione3/EsOrdinamento.java
@@ -10,6 +10,20 @@ public class EsOrdinamento {
         return posizmax;
     }
 
+
+    //metodo swap che dato un array e due posizioni scambi i valori all'interno
+    public static void swap(int[] array, int pos1, int pos2){
+        int temp = 0;
+        temp = array[pos1];
+        array[pos1] = array[pos2];
+        array[pos2] = temp;
+
+        for(int i = 0; i < array.length; i++){
+            System.out.print(array[i]);
+        }
+    }
+
+
     public static void main(String[] args) {
         int[] a = {1,6,9,-12};
         System.out.println(posMax(a));
