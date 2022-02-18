e59921668082b5d3fab7cae56f493c7331587c83
Alberto Trabucco <alberto.trabucco97@gmail.com>
2022-02-18 09:42:14 +0100

    creazione CompareArray

---
+++ es-lezione3/CompareArray.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CompareArray {
+    public static void main(String[] args){
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Inserisci due stringhe:");
+        String firstStr = keyboard.nextLine();
+        String secondStr = keyboard.nextLine();
+
+        char[] firstStrtocharArray = firstStr.toCharArray();
+        char[] secondStrtocharArray = secondStr.toCharArray();
+
+        for(int i = 0; i < firstStrtocharArray.length; i++){
+            for(int j = 0; j < secondStrtocharArray.length; j++){
+                if(firstStrtocharArray[i] == secondStrtocharArray[j]){
+                    System.out.print(firstStrtocharArray[i]+" ");
+                }
+            }  
+        }
+
+    }
+}
