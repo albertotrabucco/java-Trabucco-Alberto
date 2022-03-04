f160b95cebf259d83b19aa9287615bb8a80ec521
Alberto <alberto.trabucco97@gmail.com>
2022-03-04 13:20:46 +0100

    test intellij git

--- lezione7/interfacce/A.java
+++
@@ -1,5 +0,0 @@
-package com.engim.lezione7.interfacce;
-
-public class A implements I1{
-
-}
--- lezione7/interfacce/I1.java
+++
@@ -1,6 +0,0 @@
-package com.engim.lezione7.interfacce;
-
-public interface I1 {
-    void m1(int x);
-    int m2();
-}
